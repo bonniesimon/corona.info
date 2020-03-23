c27d00f39efb96830e6ff1ac02665c47619e9744
Bonnie Simon <bonniesimon@protonmail.com>
2020-03-24 01:56:57 +0530

    Updates

---
+++ precache-manifest.de1da1e8e92a6ee9c4b450e92a492d21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c11b349aeb4eda680eaa93e2812457",
+    "url": "/corona.info/index.html"
+  },
+  {
+    "revision": "2cdda12f01a5de149a3c",
+    "url": "/corona.info/static/css/main.30bba7ee.chunk.css"
+  },
+  {
+    "revision": "ecb7a038c4a9d0b34fc9",
+    "url": "/corona.info/static/js/2.a5b85cc7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/corona.info/static/js/2.a5b85cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cdda12f01a5de149a3c",
+    "url": "/corona.info/static/js/main.8508e6dc.chunk.js"
+  },
+  {
+    "revision": "8cb3d8e6d580a87baf56",
+    "url": "/corona.info/static/js/runtime-main.e8166be6.js"
+  }
+]);
