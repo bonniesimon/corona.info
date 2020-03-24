e4b02e8934aa363b8a55ba69f74e73f4fa256760
Bonnie Simon <bonniesimon@protonmail.com>
2020-03-24 12:20:18 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
 	return (
 		<div className="App">
 			<Home/>
+            <footer className="footer">
+                <p className="footer__text">Created by <a className="footer__link" href="github.com/bonniesimon">Bonnie Simon</a>. <a className="footer__link" href="github.com/bonniesimon/corona.info">Repo</a> </p>
+            </footer>
 		</div>
 	);
 }
